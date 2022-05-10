044be2b19841ffbd5c220c0509df50f28e766500
Nate Sire <nate.sire@fairwaymc.com>
2022-05-10 19:44:47 -0400

    npm test works but not jest

--- reactui/src/App.test.tsx
+++ reactui/src/App.test.tsx
@@ -1,3 +1,5 @@
+// https://www.newline.co/@kchan/testing-a-create-react-app-typescript-project-with-jest-and-react-testing-library--c157b34e
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
