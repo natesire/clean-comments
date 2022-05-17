787d16c1c7dec6553ab8705c1d745ad37f156fc0
Nate Sire <nate.sire@fairwaymc.com>
2022-05-17 01:56:41 -0400

    rename var

--- lib/Comments.ts
+++ lib/Comments.ts
@@ -1,37 +1,27 @@
 import * as fs from 'fs'; // going to create our own filesystem using tree data structure
 import { exec } from 'child_process'; // going to use child_process to execute commands
-
-// run system shell command
+import { parseIsolatedEntityName } from 'typescript';
 
 interface Comment {
   line: string;
-  dateStamp: string;
+  dateStamp: Date;
 }
 
-
 export class Comments implements Comment {
-
   public line: string;
-  public dateStamp: string;
+  public dateStamp: Date;
 
-  constructor(line: string) {
-    this.line = line;
+  constructor(commentText: string) {
+    this.line = commentText;
   }
 
-  command() {
+  gitBlame() {
     let command = `git blame README.md`;
-        let result = exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-            }
-     console.log(getDateStamp(stdout));
-        }
-    );
-    }
-
-    getDateStamp(line: string) {
-        let dateStamp = line.substring(0, line.indexOf(' '));
-        return dateStamp;
-    }
-    
+    let result = exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(stdout);
+    });
+  }
 }
